e93883fe9b53ea45e3d09022e16851bfaae49a29
Aleksey Gureiev <spyromus@noizeramp.com>
2013-04-04 17:59:32 +0300

    painting field

--- command/screen.js
+++ command/screen.js
@@ -1,4 +1,11 @@
-var ansi = require('ansi');
+var ansi = require('ansi'),
+    c = ansi(process.stdout);
+
+var cellWidth    = 3,
+    cellHeight   = 2,
+    cellPadding  = 1,
+    fieldOffsetX = 1,
+    fieldOffsetY = 7;
 
 function lf() { return '\n' }
 function clearScreen() {
@@ -18,6 +25,8 @@ module.exports = function(delegate) {
     renderDash(state);
     renderField(state);
     renderPlayers(state);
+
+    c.goto(1, 1);
   }
 
   var renderDash = function(state) {
@@ -25,8 +34,43 @@ module.exports = function(delegate) {
     console.log(state);
   }
 
-  var renderField = function(state) {}
-  var renderPlayers = function(state) {}
+  var renderField = function(state) {
+    var p = '   ';
+
+    var fh = state.field.h, fw = state.field.w;
+
+    for (var y = 0; y < fh; y++) {
+      for (var x = 0; x < fw; x++) {
+        for (var l = 0; l < cellHeight; l++) {
+          c.goto(x * (cellWidth + cellPadding) + fieldOffsetX, y * (cellHeight + cellPadding) + fieldOffsetY + l);
+          c.bg.grey();
+          process.stdout.write(p);
+        }
+      }
+    }
+
+    c.bg.black();
+  }
+
+  var renderPlayers = function(state) {
+    c.bg.green();
+
+    var nodes = state.nodes;
+    for (var i = 0; i < nodes.length; i++) {
+      var node = nodes[i];
+      if (node.e > 0) {
+        var x = fieldOffsetX + node.x * (cellWidth + cellPadding),
+            y = fieldOffsetY + node.y * (cellHeight + cellPadding);
+
+        c.goto(x, y);
+        process.stdout.write('   ');
+        c.goto(x, y + 1);
+        process.stdout.write(String(node.e));
+      }
+    }
+
+    c.bg.black();
+  }
 
   return self;
 }
