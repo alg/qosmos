83fea32b42c5f0a7b0df313d43816e5713cd0e26
Aleksey Gureiev <spyromus@noizeramp.com>
2013-04-05 07:26:33 +0300

    fixed broadcasting IP per workstation no need to restart the command console every time server restarts

--- server/main.js
+++ server/main.js
@@ -2,5 +2,17 @@ var Config      = require('./config'),
     GameServer  = require('./game_server'),
     gameServer  = new GameServer();
 
-Config.multicastHost = Config.multicastHost + (1 + parseInt(Math.random() * 254));
+var OS = require('os'),
+    ni = OS.networkInterfaces();
+
+for (var i in ni) {
+  var gr = ni[i];
+  for (var d in gr) {
+    var dev = gr[d];
+    if (!dev.internal && dev.family == 'IPv4') {
+      Config.multicastHost = Config.multicastHost + dev.address.match(/\.([^\.]*)$/)[1];
+    }
+  }
+}
+
 gameServer.init();
